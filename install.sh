57f32073e5bd9aa78860a2c5b39e1e802cbbf81a
Sebastian Reimers <sreimers@aoit.de>
2013-07-27 15:26:30 +0200

    update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
-# Prepare Archlinux Image
+# VARS
+pacman="pacman --noconfirm --force"
+home="/opt/studio"
+repo="git@github.com:podlove-studio-connect/webapp.git"
 
+# Install packages
+$pacman -Syu
+$pacman -S git vim
+$pacman -S nginx aiccu python2 python2-distribute avahi python2-gobject
+$pacman -S gstreamer gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-base-libs gst-plugins-bad gst-libav
+$pacman -S python2-virtualenv python2-dev alsa-plugins alsa-utils supervisor gcc make redis
 
+# Enable systemd start scripts
+systemctl enable nginx
+systemctl enable avahi-daemon
+systemctl enable supervisord
+systemctl enable redis
+
+# Create User and generate Virtualenv
+useradd --create-home --home-dir $home studio
+virtualenv --system-site-packages $home
+git clone $repo $home/webapp
+$home/bin/pip install -r $home/webapp/requirements.txt
+chown -R studio:studio $home
+
+# Cleanup
+$pacman -Scc
