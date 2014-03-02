4487a401a3047319bd2f3bf72afd97fea0ec609c
Sebastian Reimers <service@it-sreimers.de>
2014-03-02 17:11:21 +0100

    escape nginx config params

--- bootstrap.sh
+++ bootstrap.sh
@@ -159,9 +159,9 @@ http {
                     proxy_pass         http://127.0.0.1:5000;
                     proxy_redirect     off;
 
-                    proxy_set_header   Host             $host;
-                    proxy_set_header   X-Real-IP        $remote_addr;
-                    proxy_set_header   X-Forwarded-For  $proxy_add_x_forwarded_for;
+                    proxy_set_header   Host             \$host;
+                    proxy_set_header   X-Real-IP        \$remote_addr;
+                    proxy_set_header   X-Forwarded-For  \$proxy_add_x_forwarded_for;
                 }
 
                 error_page   500 502 503 504  /50x.html;
