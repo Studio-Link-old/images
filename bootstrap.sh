2ef46cabad1bf6b9f1952ebda9114210b7a476bb
Sebastian Reimers <service@it-sreimers.de>
2014-04-12 11:41:11 +0200

    fix disk free detection

--- bootstrap.sh
+++ bootstrap.sh
@@ -20,7 +20,7 @@ fi
 yes | pacman -Scc
 
 # Check disk usage
-disk_free=`df -m / | awk '{ print $4 }'`
+disk_free=`df -m / | awk '{ print $4 }' | tail -1`
 if [ $disk_free -lt 300 ]; then
     echo "Not enough free disk space [only ${disk_free} MByte free]"
     exit 1
