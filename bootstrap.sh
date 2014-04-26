7435cb1c70c0a643553549763c13a01cbe9e480a
Sebastian Reimers <service@it-sreimers.de>
2014-04-26 13:53:47 +0200

    add audio files

--- bootstrap.sh
+++ bootstrap.sh
@@ -477,6 +477,17 @@ if [ "$(uname -m)" == "armv7l" ]; then
     rm -f /tmp/*-armv7h.pkg.tar.xz
 fi
 
+# Add Audio files
+if [ ! -f /usr/local/share/baresip/ring.wav ]; then
+    mkdir -p /usr/local/share/baresip
+    cd /usr/local/share/baresip/
+    wget http://mirror.studio-connect.de/music/busy.wav
+    wget http://mirror.studio-connect.de/music/error.wav
+    wget http://mirror.studio-connect.de/music/message.wav
+    wget http://mirror.studio-connect.de/music/ring.wav
+    wget http://mirror.studio-connect.de/music/ringback.wav
+fi
+
 # Starting Services
 systemctl start redis
 systemctl start studio-webapp
