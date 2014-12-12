ca06b7cb3ca9f67d4702955eae250091c55e5210
Sebastian Reimers <service@it-sreimers.de>
2014-12-12 14:50:44 +0100

    only test commit to trigger jenkins

--- tools/test.sh
+++ tools/test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 qemu-system-arm -daemonize -nographic -M vexpress-a9 -kernel zImage -drive file=root.img,if=sd,cache=none -append "root=/dev/mmcblk0p2 rw" -m 512 -net nic -net user,hostfwd=tcp::2222-:22
 sleep 30
-ssh  -o StrictHostKeyChecking=no root@127.0.0.1 -p2222 "exit 1"
+ssh  -o StrictHostKeyChecking=no root@127.0.0.1 -p2222 "echo 'MY SSH'; exit 0"
