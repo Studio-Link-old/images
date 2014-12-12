f9b29980c6a777a2d0e2d54c67db545656e295ea
Sebastian Reimers <service@it-sreimers.de>
2014-12-12 15:10:32 +0100

    testing qemu snapshot mode

--- tools/test.sh
+++ tools/test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-qemu-system-arm -daemonize -nographic -M vexpress-a9 -kernel zImage -drive file=root.img,if=sd,cache=none -append "root=/dev/mmcblk0p2 rw" -m 512 -net nic -net user,hostfwd=tcp::2222-:22
+qemu-system-arm -daemonize -nographic -M vexpress-a9 -kernel zImage \
+	-drive file=root.img,if=sd,cache=none -append "root=/dev/mmcblk0p2 rw" \
+	-m 512 -net nic -net user,hostfwd=tcp::2222-:22 -snapshot
 sleep 30
-ssh  -o StrictHostKeyChecking=no root@127.0.0.1 -p2222 "echo 'MY SSH'; exit 0"
+ssh  -o StrictHostKeyChecking=no root@127.0.0.1 -p2222 "touch /snapshot_test"
