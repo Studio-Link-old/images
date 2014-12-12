35af0c82e62789b99f3bb0707d819d1426e3ec4b
Sebastian Reimers <service@it-sreimers.de>
2014-12-12 19:28:45 +0100

    fix qemu sound device

--- tools/test.sh
+++ tools/test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+export QEMU_AUDIO_DRV=none
 qemu-system-arm -daemonize -nographic -M vexpress-a9 -kernel zImage \
 	-drive file=root.img,if=sd,cache=none -append "root=/dev/mmcblk0p2 rw" \
 	-m 512 -net nic -net user,hostfwd=tcp::2222-:22 -snapshot
-sleep 30
+sleep 20
 ssh  -o StrictHostKeyChecking=no root@127.0.0.1 -p2222 "curl -L https://raw.githubusercontent.com/studio-link/images/master/bootstrap.sh | bash -x"
