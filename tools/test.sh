716c7a0d3fe01add93b675e3a8e0c2702b2fe741
Sebastian Reimers <service@it-sreimers.de>
2014-12-14 16:11:40 +0100

    add second bootstrap run (update test)

--- tools/test.sh
+++ tools/test.sh
@@ -5,4 +5,8 @@ $qemu -daemonize -M vexpress-a9 -kernel zImage \
 	-drive file=root.img,if=sd,cache=none -append "root=/dev/mmcblk0p2 rw" \
 	-m 512 -net nic -net user,hostfwd=tcp::2222-:22 -snapshot
 sleep 20
+echo "### FIRST RUN ###"
+ssh  -o StrictHostKeyChecking=no root@127.0.0.1 -p2222 "curl -L https://raw.githubusercontent.com/studio-link/images/master/bootstrap.sh | bash -x"
+sleep 2
+echo "### SECOND RUN ###"
 ssh  -o StrictHostKeyChecking=no root@127.0.0.1 -p2222 "curl -L https://raw.githubusercontent.com/studio-link/images/master/bootstrap.sh | bash -x"
