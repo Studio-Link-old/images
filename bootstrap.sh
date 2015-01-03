b4c67929ede7712ccbebf8efb86c801842b15556
Sebastian Reimers <info@miete-admin.de>
2015-01-03 15:17:11 +0100

    update jack_capture and baresip

--- bootstrap.sh
+++ bootstrap.sh
@@ -691,9 +691,9 @@ if [[ "$(uname -m)" =~ armv7.? ]]; then
     wget $pkg_url/opus/opus-1.1-101-armv7h.pkg.tar.xz
     wget $pkg_url/libre/libre-0.4.10-1-armv7h.pkg.tar.xz
     wget $pkg_url/librem/librem-0.4.6-1-armv7h.pkg.tar.xz
-    wget $pkg_url/baresip/baresip-14.11.0-3-armv7h.pkg.tar.xz
+    wget $pkg_url/baresip/baresip-14.11.0-4-armv7h.pkg.tar.xz
     wget $pkg_url/aj-snapshot/aj-snapshot-0.9.6-1-armv7h.pkg.tar.xz
-    wget $pkg_url/jack_capture/jack_capture-0.9.71-1-armv7h.pkg.tar.xz
+    wget $pkg_url/jack_capture/jack_capture-14.12.0-1-armv7h.pkg.tar.xz
 
     pacman -Q | grep linux-am33x
     if [ $? -eq 0 ]; then
