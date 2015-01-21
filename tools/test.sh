239b3586cf2e9210e28b899d95cbae2b3a5fd878
Sebastian Reimers <info@miete-admin.de>
2015-01-21 11:00:34 +0100

    add branch

--- tools/test.sh
+++ tools/test.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 qemu="/opt/qemu-2.2.0/arm-softmmu/qemu-system-arm"
+branch="git branch | sed -n '/\* /s///p'"
 export QEMU_AUDIO_DRV=none
 $qemu -daemonize -M vexpress-a9 -kernel zImage \
 	-drive file=root.img,if=sd,cache=none -append "root=/dev/mmcblk0p2 rw" \
 	-m 512 -net nic -net user,hostfwd=tcp::2222-:22 -snapshot
 sleep 20
 echo "### FIRST RUN ###"
-ssh  -o StrictHostKeyChecking=no root@127.0.0.1 -p2222 "curl -L https://raw.githubusercontent.com/studio-link/images/master/bootstrap.sh | bash -x"
+ssh  -o StrictHostKeyChecking=no root@127.0.0.1 -p2222 "curl -L https://raw.githubusercontent.com/studio-link/images/$branch/bootstrap.sh | bash -x"
 sleep 2
 echo "### SECOND RUN ###"
-ssh  -o StrictHostKeyChecking=no root@127.0.0.1 -p2222 "curl -L https://raw.githubusercontent.com/studio-link/images/master/bootstrap.sh | bash -x"
+ssh  -o StrictHostKeyChecking=no root@127.0.0.1 -p2222 "curl -L https://raw.githubusercontent.com/studio-link/images/$branch/bootstrap.sh | bash -x"
