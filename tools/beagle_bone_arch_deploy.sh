d8c14252ba61ce737fe0de8d49214f71e3956bb6
Sebastian Reimers <info@miete-admin.de>
2015-02-01 11:37:21 +0100

    update deploy script

--- tools/beagle_bone_arch_deploy.sh
+++ tools/beagle_bone_arch_deploy.sh
@@ -1,27 +1,17 @@
 #!/bin/bash
 
-cd /root
-wget http://archlinuxarm.org/os/omap/BeagleBone-bootloader.tar.gz
-wget http://archlinuxarm.org/os/ArchLinuxARM-am33x-latest.tar.gz
-mkdir boot
-mkdir root
-mkfs.vfat -F 16 /dev/mmcblk1p1
-mkfs.ext4 /dev/mmcblk1p2
+yes | mkfs.vfat -F 16 /dev/mmcblk1p1
+yes | mkfs.ext4 /dev/mmcblk1p2
+mkdir -p boot
+mkdir -p root
 mount /dev/mmcblk1p1 boot
 mount /dev/mmcblk1p2 root
 tar -xvf BeagleBone-bootloader.tar.gz -C boot
-tar -xf ArchLinuxARM-am33x-latest.tar.gz -C root
-
-# patch u-boot env
-cat > boot/uEnv.txt << EOF
-uenvcmd=if load mmc 0:2 \${loadaddr} /boot/zImage; then setenv mmcdev 0; run uenvboot; else setenv mmcdev 1; setenv mmcroot /dev/mmcblk1p2 rw; if load mmc 1:2 \${loadaddr} /boot/zImage; then run uenvboot; else echo No zImage found; fi; fi
-uenvboot=run loadfdt; run setmmcroot; run mmcboot
-setmmcroot=setenv mmcroot /dev/mmcblk0p2 rw
-loadfdt=load mmc \${mmcdev}:2 \${fdtaddr} /boot/dtbs/\${fdtfile}
-mmcboot=echo Booting from mmc ...; run mmcargs; bootz \${loadaddr} - \${fdtaddr}
-EOF
-
+tar -xf 15_1_0-beta.tar.gz -C root
+sync
+rm root/etc/ssh/ssh_host*
 umount boot
 umount root
 sync
+sleep 5
 poweroff
