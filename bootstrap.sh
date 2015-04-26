26afeeec913c45b2bfb23bb5a230b25b11086872
Sebastian Reimers <info@miete-admin.de>
2015-04-26 14:57:13 +0200

    fix gpio permissions with udev rules

--- bootstrap.sh
+++ bootstrap.sh
@@ -615,6 +615,11 @@ EOF
 
 fi
 
+cat > /etc/udev/rules.d/91-gpio.rules << EOF
+SUBSYSTEM=="gpio", KERNEL=="gpiochip*", ACTION=="add", PROGRAM="/bin/sh -c 'chown root:studio /sys/class/gpio/export /sys/class/gpio/unexport ; chmod 220 /sys/class/gpio/export /sys/class/gpio/unexport'"
+SUBSYSTEM=="gpio", KERNEL=="gpio*", ACTION=="add", PROGRAM="/bin/sh -c 'chown root:studio /sys%p/active_low /sys%p/direction /sys%p/edge /sys%p/value ; chmod 660 /sys%p/active_low /sys%p/direction /sys%p/edge /sys%p/value'"
+EOF
+
 systemctl daemon-reload
 
 # Enable systemd start scripts
